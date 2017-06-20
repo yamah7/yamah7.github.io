7d1fabb53cf080e7caf56e3b798b3488a5bdfd89
yamah <yamah@naver.com>
2017-06-20 14:36:56 +0900

    꽃 스크립트수정

--- asset/libs/jquery-sakura.js
+++ asset/libs/jquery-sakura.js
@@ -52,10 +52,10 @@
         var defaults = {
             blowAnimations: ['blow-soft-left', 'blow-medium-left', 'blow-hard-left', 'blow-soft-right', 'blow-medium-right', 'blow-hard-right'],
             className: 'sakura',
-            fallSpeed: 1.5,
+            fallSpeed: 1,
             maxSize: 14,
             minSize: 9,
-            newOn: 200,
+            newOn: 350,
             swayAnimations: ['sway-0', 'sway-1', 'sway-2', 'sway-3', 'sway-4', 'sway-5', 'sway-6', 'sway-7', 'sway-8']
         };
 
